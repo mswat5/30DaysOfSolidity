86c4d42b77a0fe8194b7a75bcd4d8e63a1bdaa4a
mswat5 <mswat14361@gmail.com>
2023-06-14 20:04:12 +0530

    Update Day_1.sol

--- Day_1.sol
+++ Day_1.sol
@@ -4,10 +4,4 @@ pragma solidity >=0.5.0 <0.9.0;
 
 contract HelloSolidity {
     string public str = "Hello Solidity";
-
-    //Another way to write it in a function:
-    function sayHello() public pure returns (string memory) {
-        string memory _str = "Hello Solidity";
-        return _str;
-    }
 }
