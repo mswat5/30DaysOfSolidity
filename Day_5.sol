9dd0a1293e0557c0e8e437f59678751efdbf713b
Swatantra mishra <mswat14361@gmail.com>
2023-06-19 01:06:25 +0530

    Update Day_5.sol

--- Day_5.sol
+++ Day_5.sol
@@ -1,1 +1,11 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.5.0 <0.9.0;
+
+contract day5 {
+    function find(int a) public pure returns(int){
+    require(a>0,"a shouldn't be negative"); //if condition doesn't satisfy the msg will be reverted
+    int remain = (a % 3); 
+    return remain;
+    }
+}
 
